03fbd79926010261f2d142bf3d8db8336933993f
zhouzhichao <zzc123@foxmail.com>
2015-01-12 10:34:06 +0800

    asdad:wq

---
+++ show.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/1/12
+ * Time: 9:52
+ */
+use F\S\S1 as S1;
+include './vendor/autoload.php';
+
+
+S1::info();
---
+++ src/F/S/S1.php
@@ -0,0 +1,14 @@
+<?php
+namespace F\S;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/1/12
+ * Time: 9:50
+ */
+
+class S1 {
+    public static function  info(){
+        echo "S1";
+    }
+} 
